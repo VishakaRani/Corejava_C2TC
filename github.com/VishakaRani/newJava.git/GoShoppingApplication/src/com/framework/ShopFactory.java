6a006083e2dc8a021415ffbee3dde25f1a1511f7
VISHAKA RANI <VISHAKA RANI@user>
2022-08-25 12:28:29 +0530

    first commit

---
+++ github.com/VishakaRani/newJava.git/GoShoppingApplication/src/com/framework/ShopFactory.java
@@ -0,0 +1,8 @@
+package com.framework;
+
+public interface ShopFactory {
+		public PrimeAcc getNewPrimeAcc(int AccNo, String accNm, float charges, boolean isPrime);
+		public NormalAcc getNewNormalAcc(int AccNo, String accNm, float charges, float deliveryCharges);
+	}
+
+
